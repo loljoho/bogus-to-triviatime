fdc6a25bb2298eb01907bbed153194fe53f3405e
loljoho <loljoho@users.noreply.github.com>
2019-01-12 17:25:55 -0500

    Refactor code into lineToObject function

--- index.js
+++ index.js
@@ -20,28 +20,33 @@ const USER_PROPS = [
 lineReader.on('line', (line) => {
   // read user lines only
   if (line.startsWith(':N:')) {
-    // slice beginning and end of line
-    let line_str = line.slice(3).slice(0, -4);
-    // split line into array
-    let line_arr = line_str.split(' ');
-
-    // convert line into object
-    let line_obj = {};
-    for (var i = 0; i < line_arr.length; i++) {
-      line_obj[USER_PROPS[i]] = line_arr[i];
-      // canonical usernames
-      if (i === 1) {
-        if (line_arr[i] === '*') {
-          line_obj[USER_PROPS[i]] = line_arr[0];
-        }
-      }
-    }
-
     // log to console
-    console.log(JSON.stringify(line_obj));
+    console.log(JSON.stringify(lineToObject(line)));
   } // end if
 });
 
 lineReader.on('close', () => {
   console.log('\n');
 });
+
+/**
+ * Converts line string into JS object
+ */
+const lineToObject = (line) => {
+  // slice beginning and end of line
+  let line_str = line.slice(3).slice(0, -4);
+  // split line into array
+  let line_arr = line_str.split(' ');
+  // convert array into js object
+  let line_obj = {};
+  for (var i = 0; i < line_arr.length; i++) {
+    line_obj[USER_PROPS[i]] = line_arr[i];
+    // canonical usernames
+    if (i === 1) {
+      if (line_arr[i] === '*') {
+        line_obj[USER_PROPS[i]] = line_arr[0];
+      } // end if
+    } // end if
+  } // end for
+  return line_obj;
+};
