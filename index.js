6d3ba1763da71b2d1e54b31269241677404c254b
loljoho <loljoho@users.noreply.github.com>
2019-01-12 14:59:21 -0500

    Read lines and output as JSON to console

--- index.js
+++ index.js
@@ -6,11 +6,25 @@ let instream = fs.createReadStream('./data/t2.users.example');
 let outstream = new stream;
 let lineReader = readline.createInterface(instream, outstream);
 
+const PROPS = ['username', 'name', 'hostmask', 'points_day', 'quests_day', 'points_week', 'quests_week', 'points_month', 'quests_month', 'points_made', 'num_answered', 'created', 'last_updated'];
+
 lineReader.on('line', (line) => {
   // read user lines only
   if (line.startsWith(':N:')) {
-    console.log(line);
-  }
+    // slice beginning and end of line
+    let line_str = line.slice(3).slice(0, -4);
+    // split line into array
+    let line_arr = line_str.split(' ');
+
+    // convert line into object
+    let line_obj = {};
+    for (var i = 0; i < line_arr.length; i++) {
+      line_obj[PROPS[i]] = line_arr[i];
+    }
+
+    // log to console
+    console.log(JSON.stringify(line_obj));
+  } // end if
 });
 
 lineReader.on('close', () => {
